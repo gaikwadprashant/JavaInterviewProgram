96ca5d88417a713ca0685c2e10d8a5372b46819e
gaikwad_p <mailprashantgaikwad@gmail.com>
2016-03-17 17:53:02 +0530

    remove() test

--- Exercise/Com.datastructure.examples/Colletion/IteratorTest.java
+++ Exercise/Com.datastructure.examples/Colletion/IteratorTest.java
@@ -10,19 +10,21 @@ public class IteratorTest {
 	public static void main(String[] args) {
 		List<String> list = new ArrayList<String>();
 		
-		list.add("qwwerreree");
-		list.add("qwweewrerretertere");
-		list.add("qertwe");
-		list.add("qrtwe");
-		list.add("tyqerwe");
-		list.add("tyqwe");
+		list.add("1");
+		list.add("2");
+		list.add("3");
+		list.add("4");
+		list.add("5");
+		list.add("6");
 				
 		
 		for(Iterator<String> itr = list.iterator(); itr.hasNext();)
 		{
 			System.out.println("Elements in list "+itr.next());
+			//System.out.println("Elements in list "+itr.remove());
+				itr.remove();
 			//list.add("new element added");
-			System.out.println("Elements in list after adding "+itr.next());
+			System.out.println("Elements in list after adding "+list);
 		}
 			
 	
